505ac675d42fbe95ecfc4ddff50aa8a30cf00ae5
renan <naneraruom@gmail.com>
2022-11-16 04:56:29 -0300

    feat: add env

--- aws.js
+++ aws.js
@@ -1,3 +1,9 @@
 const AWS = require('aws-sdk')
-AWS.config.update({region: 'us-east-1'});
-module.exports = { AWS }
+
+AWS.config.update({ region: process.env.REGION });
+
+const dynamoDbLocal = new AWS.Endpoint(process.env.DYNAMODB_LOCAL);
+
+const DynamoDb = new AWS.DynamoDB({ endpoint: dynamoDbLocal })
+
+module.exports = { AWS, DynamoDb }
